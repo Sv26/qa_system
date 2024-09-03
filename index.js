e5a83a462ea5cb6da3fdd2eebf132ef63cf12910
Shubham Verma <91955778+Sv26@users.noreply.github.com>
2024-09-03 20:25:08 +0530

    Update and rename index.mjs to index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var formRoutes = require('./src/routes/formRoutes'); 
+var path = require('path');
+
+var PORT = 3000;
+var app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.use('/', formRoutes);
+
+// Start the server
+app.listen(PORT, function() {
+    console.log('Server is running on port ' + PORT);
+});
